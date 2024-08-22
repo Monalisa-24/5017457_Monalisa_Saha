bcd34189e12b0576f0976b381714b4c6ca4eb228
Monalisa Saha <116990954+Monalisa-24@users.noreply.github.com>
2024-08-22 10:42:53 +0530

    Add files via upload

---
+++ Week_5/Exercise 15/BookstoreAPI/target/generated-sources/annotations/com/book/BookstoreAPI/mapper/CustomerMapperImpl.java
@@ -0,0 +1,47 @@
+package com.book.BookstoreAPI.mapper;
+
+import com.book.BookstoreAPI.dto.CustomerDTO;
+import com.book.BookstoreAPI.model.Customer;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-21T16:31:45+0530",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240725-1906, environment: Java 17.0.11 (Eclipse Adoptium)"
+)
+@Component
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Override
+    public CustomerDTO toDTO(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDTO.CustomerDTOBuilder customerDTO = CustomerDTO.builder();
+
+        customerDTO.address( customer.getAddress() );
+        customerDTO.email( customer.getEmail() );
+        customerDTO.id( customer.getId() );
+        customerDTO.name( customer.getName() );
+
+        return customerDTO.build();
+    }
+
+    @Override
+    public Customer toEntity(CustomerDTO customerDTO) {
+        if ( customerDTO == null ) {
+            return null;
+        }
+
+        Customer.CustomerBuilder customer = Customer.builder();
+
+        customer.address( customerDTO.getAddress() );
+        customer.email( customerDTO.getEmail() );
+        customer.id( customerDTO.getId() );
+        customer.name( customerDTO.getName() );
+
+        return customer.build();
+    }
+}
